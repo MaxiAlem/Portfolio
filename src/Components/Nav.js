6782b9f91f42ee827711da22c7d18ff084cc7441
MaxiAlem <maerc.maxi@gmail.com>
2023-02-03 17:43:47 -0300

    some fixes

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,25 +2,33 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <nav className='navbar bg-dark sticky-top'>
-        <div className='container text-white fs-3'>
-        <div className='col-8 container row'>
-          <p className='col text-start'> (☞ﾟヮﾟ)☞</p>
-          <p className='col mx-2'> <a href="#about"  className="link-light">
-           {'>'}About yo</a></p> 
-        <p className='col mx-2'> <a href="#projects"  className="link-light">
-           {'>'}Proyectos</a></p>  
-        <p className='col mx-2'><a href="#contact"  className="link-light">
-        {'>'}Contacto</a></p>
-           </div>
-        <div className='col-1'><p><a href="https://github.com/MaxiAlem" target="_blank"rel="noreferrer"  className="link-light">
+    <nav className='navbar bg-dark sticky-top display-6 justify-content-center'>
+      
+    <ul class="nav ">
+      <li class="nav-item text-light">
+              {'(☞ﾟヮﾟ)☞ '}
+      </li>
+      <li class="nav-item">
+        <a class="nav-link link-light" aria-current="page" href="#about">About yo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link link-light" href="#projects">Proyectos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link link-light" href="#contact">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  href="https://github.com/MaxiAlem" target="_blank"rel="noreferrer"  className="link-light">
           <img className='icon'
-         alt='github' src='../assets/gh.png'></img> </a></p></div>
-        <div className='col-1'> <p><a href="https://www.linkedin.com/in/maxialem90/" target="_blank"rel="noreferrer" className="link-light">
+         alt='github' src='../assets/gh.png'></img> </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  href="https://www.linkedin.com/in/maxialem90/" target="_blank"rel="noreferrer"  className="link-light">
           <img className='icon'
-         alt='linkedin' src='../assets/linkedin.png'></img></a></p></div>
-
-        </div>
+         alt='github' src='../assets/linkedin.png'></img> </a>
+      </li>
+    </ul>
+       
     </nav>
   )
 }
