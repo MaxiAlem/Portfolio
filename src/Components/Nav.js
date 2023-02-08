a6b8df4b00a4a719b72cdc6d0d636343dab1c08f
MaxiAlem <maerc.maxi@gmail.com>
2023-02-07 21:27:25 -0300

    some fixes

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,34 +2,54 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <nav className='navbar bg-dark sticky-top display-6 justify-content-center'>
+    // <nav className='   sticky-top display-6 justify-content-center'>
       
-    <ul class="nav ">
-      <li class="nav-item text-light">
-              {'(☞ﾟヮﾟ)☞ '}
-      </li>
-      <li class="nav-item">
-        <a class="nav-link link-light" aria-current="page" href="#about">About yo</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link link-light" href="#projects">Proyectos</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link link-light" href="#contact">Contacto</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link"  href="https://github.com/MaxiAlem" target="_blank"rel="noreferrer"  className="link-light">
-          <img className='icon'
-         alt='github' src='../assets/gh.png'></img> </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link"  href="https://www.linkedin.com/in/maxialem90/" target="_blank"rel="noreferrer"  className="link-light">
-          <img className='icon'
-         alt='github' src='../assets/linkedin.png'></img> </a>
-      </li>
-    </ul>
+    // <ul class="nav ">
+    // 
+    // </ul>
        
-    </nav>
+    // </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
+
+  <div class="container-fluid">
+
+    <a class="navbar-brand text-light" href="#">
+      {'(☞ﾟヮﾟ)☞ '}
+      </a>
+
+    <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon "></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+
+      <ul class="navbar-nav">
+     
+        <li class="nav-item">
+          <a class="nav-link link-light" aria-current="page" href="#about">About yo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link link-light" href="#projects">Proyectos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link link-light" href="#contact">Contacto</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link"  href="https://github.com/MaxiAlem" target="_blank"rel="noreferrer"  className="link-light">
+            <img className='icon'
+          alt='github' src='../assets/gh.png'></img> </a>
+        </li>
+        <li class="nav-item px-3  ">
+          <a class="nav-link"  href="https://www.linkedin.com/in/maxialem90/" target="_blank"rel="noreferrer"  className="link-light">
+            <img className='icon'
+          alt='github' src='../assets/linkedin.png'></img> </a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
   )
 }
 
