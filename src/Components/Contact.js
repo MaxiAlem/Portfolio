5517148fb32f55998654077841954843de2a5028
MaxiAlem <maerc.maxi@gmail.com>
2023-02-14 20:11:36 -0300

    some fixes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,13 +8,14 @@ const Contact = () => {
   }
   return (
     <div id='contact' className=''>
-       <div className='row container pt-5 g-3'>
-        <div className='col-8 justify-content-center  '>
+       <div className='row container g-3 position-relative '>
+        <div className='col-8 justify-content-center position-absolute top-50 start-0 pt-5'>
         
           <button type="button" onClick={()=>{downlCV()}} className="btn btn-outline-light btn-lg mx-auto my-auto ">Download CV</button>  
+          <p className='text-light'>(es gratis)</p>
           </div>
           
-        <div className='col-4 text-light  contact-icons row container '>
+        <div className='col-4 text-light  contact-icons row container position-absolute top-0 start-50 pt-5 '>
                 
           <div className='col'>
             <a href="https://www.linkedin.com/in/maxialem90/" target="_blank"rel="noreferrer" className="link-light ">
